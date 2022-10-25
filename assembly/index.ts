dbc0985e67766830b8d81187c04037a1ee3a17b0
Naynesh Raghunath Rathod <nayneshrathod@gmail.com>
2022-10-25 19:24:05 +0000

    add new Data after deploy

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,16 +1,12 @@
 import { storage, Context } from "near-sdk-as";
-
-class Greeting { 
-
-    getGreeting(accountId: string): string | null { 
-        return storage.get<string>(accountId, 
-            "Greeting not available"); 
+export class Greeting {
+    getGreeting(accountId: string): string | null {
+        return storage.get<string>(accountId,
+            "Greeting not available");
     }
 
-    setGreeting(greeting: string) { 
-
+    setGreeting(greeting: string): void {
         storage.set(Context.sender, greeting);
-
-    } 
+    }
 }
 
